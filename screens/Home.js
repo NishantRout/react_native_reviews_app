57958c1b582b86d79020e354c7b320a9ec2d95ba
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-09-22 16:14:32 +0530

    navigating around

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-function Home() {
+function Home({ navigation }) {
+    const [reviews, setReviews] = useState([
+        { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+        { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+        { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+    ]);
+
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails');
+    }
+
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Screen</Text>
+            <Button title='Go to Review' onPress={pressHandler} />
         </View>
     )
 }
